c1a8f4c03a5f6035ba5d21bc4eee7b6228b0240d
zxc88645 <zxc88645@users.noreply.github.com>
2023-05-16 22:11:46 +0800

    可接受批次

--- pdf-to-jpg.bat
+++ pdf-to-jpg.bat
@@ -1,8 +1,9 @@
-echo off
+@echo off
+setlocal enabledelayedexpansion
 echo.
 echo.
 
-set DPI=300
+set DPI=150
 set ALPHABITS=2
 set QUALITY=80
 set FIRSTPAGE=1
@@ -10,20 +11,19 @@ set LASTPAGE=9999
 REM MEMORY in MB
 set MEMORY=300
 
-cd /D %~dp1
+set GS=bin\gswin32c.exe
 
-set PDFFILE=%~nx1
-set JPGFILE=%PDFFILE:.pdf=-%%%d.jpg
+for %%A in (%*) do (
+	cd /D %%~dpA
 
-set GS=%2
-IF "%GS%" == "" (
-	set GS=bin\gswin32c.exe
+	set PDFFILE=%%~nxA
+	set JPGFILE=opt\!PDFFILE:.pdf=-%%d.jpg!
+
+	echo Loading %GS%...
+	echo.
+
+	"%~dp0%GS%" -sDEVICE=jpeg -sOutputFile=!JPGFILE! -r%DPI% -dNOPAUSE -dFirstPage=%FIRSTPAGE% -dLastPage=%LASTPAGE% -dJPEGQ=%QUALITY% -dGraphicsAlphaBits=%ALPHABITS%  -dTextAlphaBits=%ALPHABITS%  -dNumRenderingThreads=4 -dBufferSpace=%MEMORY%000000  -dBandBufferSpace=%MEMORY%000000 -c %MEMORY%000000 setvmthreshold -f !PDFFILE! -c quit
+
+	echo Finished.
 )
-
-echo Loading %GS%...
-echo.
-
-"%~dp0%GS%" -sDEVICE=jpeg -sOutputFile=%JPGFILE% -r%DPI% -dNOPAUSE -dFirstPage=%FIRSTPAGE% -dLastPage=%LASTPAGE% -dJPEGQ=%QUALITY% -dGraphicsAlphaBits=%ALPHABITS%  -dTextAlphaBits=%ALPHABITS%  -dNumRenderingThreads=4 -dBufferSpace=%MEMORY%000000  -dBandBufferSpace=%MEMORY%000000 -c %MEMORY%000000 setvmthreshold -f %PDFFILE% -c quit
-
-echo Finished.
 pause
